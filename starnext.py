52d3c94f4be0056e5a74898604a19c67c590074b
achang6 <gnahcydna@gmail.com>
2015-10-09 11:08:19 -1000

    Transferred user class to player.py module

--- starnext.py
+++ starnext.py
@@ -66,38 +66,6 @@ def game_cycle():
 
    
 
-class User(pygame.sprite.Sprite):
-    def __init__(self, image, x, y):
-        self.image = image
-        self.rect     = self.image.get_rect()
-        self.rect.x   = x
-        self.rect.y   = y
-        self.x_shift  = 0
-        self.y_shift  = 0
-        self.walls    = None
-    def accelerate(self, x, y):
-        self.x_shift += x
-        self.y_shift += y
-    def update(self):
-        # horizontal
-        self.rect.x += self.x_shift
-        wall_collision_container = pygame.sprite.spritecollide(self, self.walls, False)
-        for wall in wall_collision_container:
-            if self.x_shift > 0:
-                self.rect.right = wall.rect.left
-            elif self.x_shift < 0:
-                self.rect.left = wall.rect.right
-        # vertical
-        self.rect.y += self.y_shift
-        wall_collision_container = pygame.sprite.spritecollid(self, self.walls, False)
-        for wall in wall_collision_container:
-            if self.y_shift > 0:
-                self.rect.bottom = wall.rect.top
-            elif self.y_shift < 0:
-                self.rect.top = wall.rect.bottom
-
-
-
 
 
 

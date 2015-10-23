a61f464c26855a1bd8a528fd22b3ebf9ee4ed5e3
achang6 <gnahcydna@gmail.com>
2015-10-23 07:43:21 -1000

    putting project on hiatus, have to work on tutorials. Gonna start from scratch

--- starfun.py
+++ starfun.py
@@ -3,38 +3,41 @@ import os
 from wallconstants import *
 import platform
 import readfiles
+import user
 
 
-
-# create image dictionary
-sprite_lib = readfiles.Readimagesfile('starnext_images.txt')
 # create levels object
 levels = readfiles.Readmapsfile('starPusherLevels.txt')
 
 
-
-def plats_assemble(lvl = 0, platforms, sprites):
+def plats_assemble(lvl = 0):
     lvl_width  = levels[lvl]['width']
     lvl_height = levels[lvl]['height']
     h_width    = lvl_width / 2 * TILEW
     h_height   = lvl_height / 2 * TILEF
 
-    platforms.empty()
-    sprites.empty()
-
-    for x in range(width):
-        for y in range(height):
+    for x in range(lvl_width):
+        for y in range(lvl_height):
             if levels[lvl]['map_object'][x][y] == '#':
-                plat_x = TILEW * x + HWINW - h_width  + HTILEW
-                plat_y = TILEF * y + HWINH - h_height + HTILEF
+                plat_x = x * TILEW + HWINW - h_width  # + HTILEW
+                plat_y = y * TILEF + HWINH - h_height # + HTILEF
 
                 plats = platform.Platform((plat_x, plat_y), sprite_lib['corner'])
 
-                platforms.add(plats)
-                sprite_list.add(plats)
 
-def level_changed(old_lvl, new_lvl):
+def lvl_changed(old_lvl, new_lvl):
     if old_lvl == new_lvl:
         return old_lvl, False
     if old_lvl != new_lvl:
         return new_lvl, True
+
+def activate_monkey(lvl = 0):
+    hlvlw = levels[lvl]['width']  / 2
+    hlvlh = levels[lvl]['height'] / 2
+    
+    monkey_x = levels[lvl]['xy_state']['user'][0]
+    monkey_x = monkey_x * TILEW + HWINW - hlvlw
+    monkey_y = levels[lvl]['xy_state']['user'][1]
+    monkey_y = monkey_y * TILEH + HWINH - hlvlh
+
+    return monkey_x, monkey_y
